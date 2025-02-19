65aceef39947f7a17ba6612c6aff2eea0987faed
pberton <1789738+pberton@users.noreply.github.com>
2025-02-19 00:00:43 -0800

    include go code

---
+++ cmd/licensing-example-go/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/omnistrate-oss/omnistrate-licensing-sdk-go/pkg/validator"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	// Validate license key for product
+	err := validator.ValidateLicenseForProduct("PRODUCT-DEV-SKU")
+	if err != nil {
+		// Print error information in html format
+		fmt.Fprintf(w, "<h1>Error</h1><p>%s</p>", err.Error())
+		return
+	}
+	// Print success message in html format
+	fmt.Fprintf(w, "<h1>Success</h1><p>License is valid</p>")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
