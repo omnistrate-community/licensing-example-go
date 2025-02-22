cd18877b2a53a77e461436fcb4f862b1cc9fad87
pberton <1789738+pberton@users.noreply.github.com>
2025-02-21 18:16:49 -0800

    update example for production

--- cmd/licensing-example-go/main.go
+++ cmd/licensing-example-go/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/omnistrate-oss/omnistrate-licensing-sdk-go/pkg/validator"
 )
@@ -12,13 +13,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	// Validate license key for product
 
 	// This is the validation method that can be used to validate license key for a product in Omnistrate
-	// err := validator.ValidateLicenseForProduct("PRODUCT-DEV-SKU")
-
-	// Using options to make it work with Omnistrate pre prod environment
-	err := validator.ValidateLicenseWithOptions(validator.ValidationOptions{
-		SKU:               "PRODUCT-DEV-SKU",
-		CertificateDomain: "licensing.omnistrate.dev",
-	})
+	err := validator.ValidateLicenseForProduct("PRODUCT-SAMPLE-SKU-UNIQUE-VALUE")
 	if err != nil {
 		// Print error information in html format
 		fmt.Fprintf(w, "<h1>Error</h1><p>%s</p>", err.Error())
@@ -30,22 +25,22 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", handler)
-	// certFile := "/etc/tls/tls.crt"
-	// keyFile := "/etc/tls/tls.key"
+	certFile := "/etc/tls/tls.crt"
+	keyFile := "/etc/tls/tls.key"
 
-	// // check if the certificate and key files exist
-	// if _, err := os.Stat(certFile); os.IsNotExist(err) {
-	// 	log.Printf("Starting server on :8443 with TLS...")
-	// 	err := http.ListenAndServeTLS(":8443", certFile, keyFile, nil)
-	// 	if err != nil {
-	// 		log.Fatalf("Server failed to start: %v", err)
-	// 	}
-	// } else {
-	// start server without TLS
-	log.Printf("Starting server on :8080 without TLS...")
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		log.Fatalf("Server failed to start: %v", err)
+	// check if the certificate and key files exist
+	if _, err := os.Stat(keyFile); os.IsNotExist(err) {
+		log.Printf("Starting server on :8443 with TLS...")
+		err := http.ListenAndServeTLS(":8443", certFile, keyFile, nil)
+		if err != nil {
+			log.Fatalf("Server failed to start: %v", err)
+		}
+	} else {
+		// start server without TLS
+		log.Printf("Starting server on :8080 without TLS...")
+		err := http.ListenAndServe(":8080", nil)
+		if err != nil {
+			log.Fatalf("Server failed to start: %v", err)
+		}
 	}
-	// }
 }
